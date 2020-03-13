e588ccbc465b903d4d28b665a23d156455b89d17
chaibin0 <kukulza0@naver.com>
2020-03-13 20:49:22 +0900

    modify package order

---
+++ src/main/java/learn/recursion1/swapNodesInPairs/Solution.java
@@ -0,0 +1,32 @@
+package learn.recursion1.swapNodesInPairs;
+
+
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int x) {
+        val = x;
+    }
+}
+
+public class Solution {
+    public ListNode swapPairs(ListNode head) {
+        helper(head);
+        return head;
+    }
+
+    public ListNode helper(ListNode item) {
+
+        if (item == null || item.next == null) {
+            return item;
+        }
+        ListNode temp1 = item.next;
+        ListNode temp2 = item.next.next;
+        temp1.next = item;
+        item = temp2;
+
+        helper(temp2);
+        return temp1;
+    }
+}

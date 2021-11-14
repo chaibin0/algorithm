12b5d2ca9a72356f326f0602c2aba55d20eaead6
chaibin0 <chaibin0520@gmail.com>
2021-11-14 19:59:33 +0900

    ADD Algorithm (2021-11-14) - 밀려버린 알고리즘 업로드

---
+++ src/main/java/problems/q61/Solution.java
@@ -0,0 +1,80 @@
+package problems.q61;
+
+
+import java.util.Objects;
+
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {
+    }
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
+
+class Solution {
+    public ListNode rotateRight(ListNode head, int k) {
+
+        if (Objects.isNull(head)) {
+            return null;
+        }
+
+        int headSize = size(head);
+
+        if (headSize == 1) {
+            return head;
+        }
+
+        int boundaryIndex = k % headSize;
+
+        if (boundaryIndex == 0) {
+            return head;
+        }
+
+        ListNode position = head;
+        ListNode front;
+        ListNode behind = null;
+
+        if (boundaryIndex != 0) {
+            behind = head;
+        }
+
+        for (int i = 0; i < headSize - boundaryIndex - 1; i++) {
+            position = position.next;
+        }
+
+        front = position.next;
+        position.next = null;
+        position = front;
+
+        for (int i = 0; i < boundaryIndex - 1; i++) {
+            position = position.next;
+        }
+
+        position.next = behind;
+        return front;
+    }
+
+    public int size(ListNode head) {
+
+        if (Objects.isNull(head)) {
+            return 0;
+        }
+
+        int count = 1;
+        while (head.next != null) {
+            count++;
+            head = head.next;
+        }
+
+        return count;
+    }
+}

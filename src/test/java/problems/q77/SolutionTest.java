608e8bff672924baf98d79eba4ba621482510e7d
chaibin0 <chaibin0520@gmail.com>
2021-12-26 21:36:09 +0900

    ADD LeetCode Algorithm(74, 77) (2021-12-26)

---
+++ src/test/java/problems/q77/SolutionTest.java
@@ -0,0 +1,15 @@
+package problems.q77;
+
+import org.junit.jupiter.api.Test;
+
+class SolutionTest {
+
+    @Test
+    void combine() {
+
+        int n = 4;
+        int k = 2;
+
+        System.out.println(new Solution().combine(n, k));
+    }
+}

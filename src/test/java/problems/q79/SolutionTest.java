44765f374875eb5901963fe63adc92390a0a46ad
chaibin0 <chaibin0520@gmail.com>
2022-01-02 23:14:36 +0900

    ADD LeetCode Algorithm(78, 79) (2021-01-02)

---
+++ src/test/java/problems/q79/SolutionTest.java
@@ -0,0 +1,26 @@
+package problems.q79;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+class SolutionTest {
+
+    @Test
+    void exist1() {
+
+        char[][] board = {{'A', 'B', 'C', 'E'}, {'S', 'F', 'C', 'S'}, {'A', 'D', 'E', 'E'}};
+        String word = "SEE";
+
+        assertThat(new Solution().exist(board, word)).isTrue();
+    }
+
+    @Test
+    void exist2() {
+
+        char[][] board = {{'A', 'B', 'C', 'E'}, {'S', 'F', 'E', 'S'}, {'A', 'D', 'E', 'E'}};
+        String word = "ABCESEEEFS";
+
+        assertThat(new Solution().exist(board, word)).isTrue();
+    }
+}

3564c65e27a26801aaa27bb404a7e13faa84aab0
chaibin0 <chaibin0520@gmail.com>
2022-03-21 21:55:34 +0900

    ADD LeetCode Algorithm(88) (2022-03-21)

---
+++ src/test/java/q88/SolutionTest.java
@@ -0,0 +1,22 @@
+package q88;
+
+import org.junit.jupiter.api.Test;
+
+class SolutionTest {
+
+    @Test
+    void merge() {
+        int[] nums1 = {1, 2, 3, 4, 5};
+        int[] nums2 = {};
+
+        new Solution().merge(nums1, 5, nums2, 0);
+    }
+
+    @Test
+    void merge2() {
+        int[] nums1 = {1,2,3,0,0,0};
+        int[] nums2 = {2,5,6};
+
+        new Solution().merge(nums1, 3, nums2, 3);
+    }
+}

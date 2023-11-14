e839cc35d957eeda0ab6288dc1b8728b812abcd0
chaibin <chaibin0520@gmail.com>
2023-11-14 21:55:18 +0900

    update leetcode 2300

--- src/main/java/leetcode/problems/q2300/Solution.java
+++ src/main/java/leetcode/problems/q2300/Solution.java
@@ -4,36 +4,34 @@ import java.util.Arrays;
 
 class Solution {
     public int[] successfulPairs(int[] spells, int[] potions, long success) {
-        int[] atLeastSuccessNum = new int[potions.length];
 
         for (int i = 0; i < potions.length; i++) {
-            atLeastSuccessNum[i] = (int) Math.ceil((double) success / potions[i]);
+            potions[i] = (int) Math.ceil((double) success / potions[i]);
         }
 
-        Arrays.sort(atLeastSuccessNum);
+        Arrays.sort(potions);
 
-        int[] answer = new int[spells.length];
         for (int i = 0; i < spells.length; i++) {
             int spell = spells[i];
             int low = 0;
-            int high = atLeastSuccessNum.length - 1;
-            int mid = 0;
+            int high = potions.length - 1;
+            int mid;
             while (low <= high) {
                 mid = (low + high) / 2;
 
-                if (spell >= atLeastSuccessNum[mid]) {
+                if (spell >= potions[mid]) {
                     low = mid + 1;
                 } else {
                     high = mid - 1;
                 }
             }
 
-            if (low != atLeastSuccessNum.length && atLeastSuccessNum[low] < spell) {
+            if (low != potions.length && potions[low] < spell) {
                 low++;
             }
-            answer[i] = low;
+            spells[i] = low;
         }
 
-        return answer;
+        return spells;
     }
 }
